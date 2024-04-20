ca1d6d26b00a3ebfd9b1453c8246d92142f06ac7
nickanorkill05 <nickanor.nc@gmail.com>
2024-04-20 15:30:18 +0800

    Clerk login-sign_up

---
+++ website/Shadcn/clerk/clerk/src/app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashBoardPage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1>DashBoard</h1>
+    </div>
+  )
+}
+
+export default DashBoardPage
---
+++ website/Shadcn/clerk/clerk/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid h-screen place-items-center bg-gray-700">  
+    Home Page!!
+    </div>
+  );
+}
